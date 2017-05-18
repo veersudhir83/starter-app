250aeb4cd2ce3b36e4ca595f9a5d386adb5e1484
Sudheer Kumar Veeravalli <veersudhir83@gmail.com>
2017-05-18 11:06:58 +0530

    Add files via upload

---
+++ src/main/java/io/sudheer/springboot/topic/TopicController.java
@@ -0,0 +1,20 @@
+package io.sudheer.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring", "Springframework", "Springframework Description"),
+				new Topic("java", "Core Java", "Core Java description"),
+				new Topic("javascript", "Javascript", "Javascript Description")
+				);
+	}
+}

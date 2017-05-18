192ef0419ac17c55b4a3aec10123c8ddc412f987
502437115 <sudheer.veeravalli@ge.com>
2017-05-18 15:48:50 +0530

    Added full implementation for Topic - Service and Controller

--- src/main/java/io/sudheer/springboot/topic/TopicController.java
+++ src/main/java/io/sudheer/springboot/topic/TopicController.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
 	@RequestMapping("/allTopics")
-	public List<Topic> getAllTopics() { //gets all topics from static array list within the controller class
+	//gets all topics from static array list within the controller class
+	public List<Topic> getAllTopics() { 
 		return Arrays.asList(
 				new Topic("spring", "Springframework", "Springframework Description"),
 				new Topic("java", "Core Java", "Core Java description"),
@@ -23,7 +27,32 @@ public class TopicController {
 	private TopicService topicService;
 	
 	@RequestMapping("/topics")
-	public List<Topic> getTopics() { //gets all topics from static arraylist within the controller class
-		return topicService.getAllTopics();
+	//gets all topics using service
+	public List<Topic> getTopics() { 
+		return topicService.getTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	//gets the topic using service
+	public Topic getTopic(@PathVariable("id") String id) { 
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	//adds the topic using service
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	//updates the topic using service
+	public void updateTopic(@RequestBody Topic topic, @PathVariable("id") String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	//Deletes the topic using service
+	public void removeTopic(@PathVariable("id") String id) {
+		topicService.removeTopic(id);
 	}
 }
--- src/main/java/io/sudheer/springboot/topic/TopicService.java
+++ src/main/java/io/sudheer/springboot/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.sudheer.springboot.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,13 +8,50 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	List<Topic> topics = Arrays.asList(
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Springframework", "Springframework Description"),
 			new Topic("java", "Core Java", "Core Java description"),
 			new Topic("javascript", "Javascript", "Javascript Description")
-			);
+			));
 
-	public List<Topic> getAllTopics() {
+	public List<Topic> getTopics() {
 		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		System.out.println("Topic Added");
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		Topic t = null;
+		for(int i=0; i < topics.size(); i++) {
+			t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				System.out.println("Topic Updated");
+				return;
+			}
+		}
+	}
+	
+	public void removeTopic(String id) {
+		//Using Lambda predicate
+		topics.removeIf(t -> t.getId().equals(id));
+		
+		//General for loop
+		/*Topic t = null;
+		for(int i=0; i < topics.size(); i++) {
+			t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.remove(i);
+				System.out.println("Topic Deleted");
+				return;
+			}
+		}*/
 	}
 }
